b2b3e8c66ebfcfdff85f0996477deb0dcc617b3e
Jesús Torrejón <55881352+gestorHan@users.noreply.github.com>
2020-08-21 03:07:46 -0500

    Segunda version de lambdas

--- model.py
+++ model.py
@@ -1,23 +1,28 @@
 import numpy as np
 import itertools
-#m <- numero de caracteristicas
-#l <- nivel actual
-import itertools
-def findsubsets(shuffled,l , m):
-    return list(set(itertools.combinations(shuffled, l)))[:m]
 
-def generar_lambdas(l,m):
-    shuffled = np.arange(m)
-    if l == 1:
-        return list(set(itertools.combinations(shuffled,1)))
+def generar_lambdas(level, m_parametros):
+    arr_range = np.arange(m_parametros)
     rng = np.random.default_rng()
-    rng.shuffle(shuffled)
-    print (shuffled)
-    return findsubsets (shuffled,l,m)
+    lista_combinaciones = list(set(itertools.combinations(arr_range ,level)))
+    if (level == 1):
+        return lista_combinaciones
+    rng.shuffle(lista_combinaciones)    
+    print(lista_combinaciones[:m_parametros])
+    return lista_combinaciones[:m_parametros]
+
+#def vecto_t ()    
+
+def generar_tabla(bin_matrix_Q, labels, lambda_actual):
+    print ("Lambda actual : " , lambda_actual)
+    for row_i in lambda_actual:
+        print("Obtener la fila :" , row_i) 
+       
+    return []
 
 if __name__ == "__main__":
 
-    Q = np.array(
+    bin_matrix_Q = np.array(
         [
             [-1, 1, 1, -1, -1, -1, -1, -1, -1, 1, ],
             [-1, 1, -1, -1, -1, -1, -1, -1, -1, 1, ],
@@ -25,8 +30,18 @@ if __name__ == "__main__":
             [-1, 1, 1, -1, -1, -1, -1, -1, -1, -1, ],
         ]
     )
-    m , n = Q.shape
-    print (m)
-    print (n)
-    print(generar_lambdas(2,30))
-    
+    labels = np.array(
+        [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, ]
+    )
+    m_parametros, n_puntos =  bin_matrix_Q.shape
+    print(m_parametros)
+    print(n_puntos)
+    arreglo_lambdas = []
+    lambda_actual = []
+    tabla_actual = []
+    for level in range(1, 5):
+        arreglo_lambdas.append(generar_lambdas(level, m_parametros))
+        lambdas_actuales = arreglo_lambdas[level-1]
+        for lambda_actual in lambdas_actuales:
+            tabla_actual = generar_tabla(bin_matrix_Q,labels,lambda_actual)
+    print(arreglo_lambdas)
