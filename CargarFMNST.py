74185e63745c1b6362b603647562ed30f73f763a
Cesar <ccoloradoc@uni.pe>
2020-08-22 03:23:50 -0500

    Centrar Datos Listo
    Centré los datos de cada fila,
    saqué la media lo acomodé en cada MNST

--- CargarFMNST.py
+++ CargarFMNST.py
@@ -27,9 +27,52 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=train_sampl
 num = 100
 images = X_train [: num] 
 labels = y_train [: num]
-
+'''
 for i in range(100):
     l1_plot = plt.subplot(10, 10, i + 1)#Num de filas,Num de columnas, indice 
     l1_plot.imshow(images[i].reshape(28, 28),cmap=plt.cm.gray)
 
 plt.show()
+'''
+
+print(images.shape)
+#PASAR EL ARRELGO TRIDIMENSIONAL A BIDIMENSIONAL
+#images es de 60x(28*28)
+'''
+for i in range(300,400):
+    print(images.item(25,i))
+'''
+images = np.transpose(images)
+
+#images es de (784)x60
+print(images.shape)
+'''
+for i in range(300,400):
+    print(images[i][25])
+'''
+#Creo un arreglo de ceros [784]
+median = np.zeros(784)
+t=0
+#sumo toda la fila de mis elementos a cada posicion
+for i in range(784):
+    for j in range(60):
+        median[t]=median[t]+images[i][j]
+    t=t+1
+for i in range(300,400):
+    print(images[i][25])
+#saco la media a cada suma hallada
+for i in range(784):
+    median[i]=median[i]/60
+
+'''
+for i in range(784):
+    print(median[i])
+'''
+#vamos a restar a cada valor la media en la fila correspondiente:
+for i in range(784):
+    for j in range(60):
+        images[i][j] = images[i][j] - median[i]
+'''
+for i in range(300,400):
+    print(images[i][25])
+'''
